ee0cdbbde7cf6cbcc88820fc5cfd74cd302140da
ashutoshsahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2024-08-25 23:21:12 +0530

    update sb version

--- src/main/java/com/ashu/practice/SpringBootCircuitBreakerResilience4jApplication.java
+++ src/main/java/com/ashu/practice/SpringBootCircuitBreakerResilience4jApplication.java
@@ -1,14 +1,38 @@
 package com.ashu.practice;
 
 
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.HandlerMethod;
 
 @SpringBootApplication
 public class SpringBootCircuitBreakerResilience4jApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCircuitBreakerResilience4jApplication.class, args);
+    }
+
+
+//    @Bean
+    public OperationCustomizer customGlobalHeaders() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            // Add a header to each request in swagger-ui
+            Parameter missingParam3 = new Parameter()
+                    .in(ParameterIn.HEADER.toString())
+                    .schema(new StringSchema())
+                    .name("header3")
+                    .required(true)
+                    .description("Example Header")
+                    .example("header3");
+            operation.addParametersItem(missingParam3);
+            return operation;
+        };
     }
 
 }
